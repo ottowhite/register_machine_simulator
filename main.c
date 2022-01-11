3a0faef321c0e32b505ec25a60d31fc75c564aa9
Otto White <whiteotto4@gmail.com>
2022-01-11 19:38:37 +0000

    [OW / feat] set up execution demonstration with argument

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #include "execute.h"
 #include "pretty_print.h"
 
-static void demonstrate(void);
+static void demonstrate_execution(void);
 static void demonstrate_translation(int input);
 static void print_usage(void);
 
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
       demonstrate_translation(atoi(argv[2]));
     } else if (!strcmp(argv[1], "-d") && argc == 2) {
       /* Demonstration */
-      demonstrate();
+      demonstrate_execution();
     } else {
       /* Invalid argument */
       print_usage();
@@ -46,34 +46,35 @@ static void demonstrate_translation(int input) {
 
 }
 
-static void demonstrate(void) {
-
-  
+static void demonstrate_execution(void) {
   /* RM program execution. */
   int configuration[4] = {0, 0, 7, -1};
 
-  struct body program[20];
-  program[0] = (struct body) { SUBTRACT,  1,  2,  1 };
-  program[1] = (struct body) { HALT                 };
-  program[2] = (struct body) { SUBTRACT,  1,  3,  4 };
-  program[3] = (struct body) { SUBTRACT,  1,  5,  4 };
-  program[4] = (struct body) { HALT                 };
-  program[5] = (struct body) { ADD,       0,  0     };
+  struct body program[20] = {
+    (struct body) { SUBTRACT,  1,  2,  1 },
+    (struct body) { HALT                 },
+    (struct body) { SUBTRACT,  1,  3,  4 },
+    (struct body) { SUBTRACT,  1,  5,  4 },
+    (struct body) { HALT                 },
+    (struct body) { ADD,       0,  0     }
+  };
 
-  int encoded_program_arr[20];
-  encoded_program_arr[0] = encode_body_to_int (program[0]);  
-  encoded_program_arr[1] = encode_body_to_int (program[1]);
-  encoded_program_arr[2] = encode_body_to_int (program[2]);
-  encoded_program_arr[3] = encode_body_to_int (program[3]);
-  encoded_program_arr[4] = encode_body_to_int (program[4]);
-  encoded_program_arr[5] = encode_body_to_int (program[5]);
-  encoded_program_arr[6] = -1;
+  int encoded_program_arr[20] = {
+    encode_body_to_int (program[0]),
+    encode_body_to_int (program[1]),
+    encode_body_to_int (program[2]),
+    encode_body_to_int (program[3]),
+    encode_body_to_int (program[4]),
+    encode_body_to_int (program[5]),
+    -1
+  };
 
-  printf ("\nProgram from encoded array, decoded\n");
+  printf ("Program from encoded array, decoded\n");
   for (int i = 0; encoded_program_arr[i] != -1; i++) {
     printf ("L%d: ", i);
     print_body (decode_int_to_body(encoded_program_arr[i]));
   }
+  printf("\n");
 
   execute_program (configuration, program);
 }
@@ -82,7 +83,5 @@ static void print_usage(void) {
   printf(
       "Usage: ./main OPTION [ARGS..]\n\n"
       "   -g: Translate a given godel number into a register machine. \n"
-      "   -e: Execute the register machine represented by a given Godel number \n" 
-      "       with a given -1 terminated initial configuration. \n"
-      "   -d: Perform a demonstration. \n");
+      "   -d: Perform an execution trace on a hard coded register machine. \n");
 }
